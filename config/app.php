97fd051c54cb55d8e7ba1773ada6723f520ef466
nothing628 <yogyphang@gmail.com>
2016-08-04 23:12:54 +0700

    register users facade

--- config/app.php
+++ config/app.php
@@ -203,6 +203,7 @@ return [
 		'Validator' => Illuminate\Support\Facades\Validator::class,
 		'View' => Illuminate\Support\Facades\View::class,
 
+		'Users' => ICMS\Facades\UserFacade::class,
 		'Theme' => ICMS\Facades\ThemeFacade::class,
 		'Menu' => ICMS\Facades\MenuFacade::class,
 		'Package' => ICMS\Facades\PackageFacade::class,
