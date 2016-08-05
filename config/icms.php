672c289342525149717c2b3088f83610f45f9bca
nothing628 <yogyphang@gmail.com>
2016-08-05 14:36:52 +0700

    join menu config to icms

--- config/icms.php
+++ config/icms.php
@@ -4,6 +4,11 @@ return [
 	'template' => [
 		'admin' => 'layouts.base.admin',
 		'web'	=> 'layouts.base.web',
+	],
+	'package_path' => 'package',
+	'menu_container' => [
+		'container' => 'layouts.menu.container',
+		'parent'	=> 'layouts.menu.parent',
+		'child'		=> 'layouts.menu.child',
 	]
-	
 ];
