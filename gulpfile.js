2ab43664028c5e2decfd8845a67e70dd3bd6923b
nothing628 <yogyphang@gmail.com>
2016-08-19 22:09:04 +0700

    create vue component

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,11 @@ var elixir = require('laravel-elixir');
  |
  */
 
+elixir.config.js.browserify.transformers.push({
+    name: 'vueify',
+    options: {}
+});
+
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.browserify('main.js');
 });
